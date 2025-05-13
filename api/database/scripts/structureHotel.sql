e630e2cbad7a5c58cc28ad4a3686a1985018e9ac
Danyblas27 <dblasquezchacon@gmail.com>
2025-05-13 12:44:25 -0600

    agrege correccion en la base de datos (claves foraneas)

--- api/database/scripts/structureHotel.sql
+++ api/database/scripts/structureHotel.sql
@@ -66,47 +66,4 @@ CREATE TABLE
 SET
   = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
--- Tabla: BookingModel
-CREATE TABLE
-  bookings (
-    id INT(11) PRIMARY KEY AUTO_INCREMENT,
-    entry_date DATE NOT NULL,
-    departure_date DATE NOT NULL,
-    status ENUM ('Pending', 'Confirmed', 'Cancelled', 'Completed') NOT NULL,
-    room_id CHAR(36),
-    client_id CHAR(36),
-    user_id CHAR(36),
-    total_amount DECIMAL(10, 2),
-    id_doc_official VARCHAR(50),
-    pay_method_id CHAR(36),
-    FOREIGN KEY (room_id) REFERENCES rooms (id),
-    FOREIGN KEY (client_id) REFERENCES clients (id),
-    FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (pay_method_id) REFERENCES pay_methods (id)
-  ) ENGINE = InnoDB DEFAULT CHARACTER
-SET
-  = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-
--- Tabla: AdditionalServiceModel
-CREATE TABLE
-  additional_services (
-    id INT(11) PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100),
-    description TEXT,
-    price DECIMAL(10, 2)
-  ) ENGINE = InnoDB DEFAULT CHARACTER
-SET
-  = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-
--- Tabla: AdditionalServicesBookingModel
-CREATE TABLE
-  additional_services_booking (
-    id INT(11) PRIMARY KEY AUTO_INCREMENT,
-    booking_id CHAR(36),
-    additional_service_id CHAR(36),
-    price DECIMAL(10, 2),
-    FOREIGN KEY (booking_id) REFERENCES bookings (id),
-    FOREIGN KEY (additional_service_id) REFERENCES additional_services (id)
-  ) ENGINE = InnoDB DEFAULT CHARACTER
-SET
-  = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+  

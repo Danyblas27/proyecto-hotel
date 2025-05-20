6f428c6fce624b82d850464ec2818e9f9c8f8a61
Danyblas27 <dblasquezchacon@gmail.com>
2025-05-19 21:47:23 -0600

    Agruegué lineas a app.js para poder hacer fetch desde live server

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ dotenv.config();
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
 //? Middlewares global
 app.use(express.json());
 app.use(cookieParser());
